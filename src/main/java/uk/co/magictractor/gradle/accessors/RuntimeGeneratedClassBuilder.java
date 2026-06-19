3770f5fc671c5f793d5558fc31e0fa0144e693a8
Ken D <github@magictractor.co.uk>
2026-06-19 08:54:57 +0100

    RuntimeGeneratedClassBuilder - introduce ChangeClassVisitor

--- src/main/java/uk/co/magictractor/gradle/accessors/RuntimeGeneratedClassBuilder.java
+++ src/main/java/uk/co/magictractor/gradle/accessors/RuntimeGeneratedClassBuilder.java
@@ -21,7 +21,6 @@ import java.io.UncheckedIOException;
 import java.lang.classfile.ClassBuilder;
 import java.lang.classfile.ClassElement;
 import java.lang.classfile.ClassFile;
-import java.lang.classfile.ClassFile.ConstantPoolSharingOption;
 import java.lang.classfile.ClassFileElement;
 import java.lang.classfile.ClassModel;
 import java.lang.classfile.CodeBuilder;
@@ -30,19 +29,19 @@ import java.lang.classfile.CodeModel;
 import java.lang.classfile.MethodBuilder;
 import java.lang.classfile.MethodElement;
 import java.lang.classfile.MethodModel;
-import java.lang.classfile.attribute.SourceFileAttribute;
-import java.lang.classfile.constantpool.ClassEntry;
-import java.lang.classfile.constantpool.ConstantPoolBuilder;
-import java.lang.classfile.constantpool.FieldRefEntry;
 import java.lang.classfile.constantpool.PoolEntry;
-import java.lang.classfile.constantpool.Utf8Entry;
-import java.lang.classfile.instruction.FieldInstruction;
-import java.lang.classfile.instruction.InvokeInstruction;
-import java.lang.classfile.instruction.LocalVariable;
 import java.lang.constant.ClassDesc;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 
 import org.gradle.internal.impldep.com.google.common.base.Strings;
 import org.gradle.internal.impldep.com.google.common.io.ByteStreams;
+
+import uk.co.magictractor.gradle.classvisitor.ChangeClassVisitor;
+import uk.co.magictractor.gradle.classvisitor.ClassFileElementVisitor;
 
 /**
  * Builder that copies a given class, transforms it using
@@ -55,18 +54,6 @@ public final class RuntimeGeneratedClassBuilder {
 
     private Class<?> templateClass;
     private String generatedClassName = "uk.co.magictractor.Play";
-
-    // User defined using withXxx() methods.
-    private String templateClassType;
-    //private String generatedClassType;
-
-    // Created when building. These might get moved to a context (or something)?
-    private transient ClassDesc _generatedClassDesc;
-    // ClassEntry describes an entry in the constant pool.
-    // Instructions using the templateClassEntry need to be changed to use the generatedClassEntry.
-    private transient ClassEntry _generatedClassEntry;
-    private transient ClassDesc _templateClassDesc;
-    private transient ClassEntry _templateClassEntry;
 
     public RuntimeGeneratedClassBuilder(Class<?> templateClass) {
         this.templateClass = templateClass;
@@ -84,94 +71,19 @@ public final class RuntimeGeneratedClassBuilder {
         }
     }
 
-    // Transforms required:
-    //
-    // Change constants in code
-    //   LoadConstant[OP=LDC, val=mockito]
-    //
-    // Change method name
-    //   MethodModel[methodName=getTemplate, methodType=()Ljava/lang/Object;, flags=1]
-    //
-    // Maybe add synthetic flag to class and generated methods
-    //  MethodModel[methodName=get, methodType=(Ljava/lang/String;)Ljava/lang/Object;, flags=1]
-    //
-    // Add marker interface with const that points to docs. (configurable)
-    //   Interfaces[interfaces=]
-    //
-    // Write info to writer (similar to initial printlns)
-    // Add java version for ClassFileVersion[majorVersion=69, minorVersion=0]
-    // Add desc for AccessFlags[flags=33]
     private Class<?> buildClass0() throws IOException, ClassNotFoundException {
-        // Don't want leading "L" or trailing semicolon. Those might be needed need to be added in some contexts.
-        templateClassType = templateClass.getName().replace('.', '/');
-        //generatedClassType = generatedClassName.replace('.', '/');
-
         byte[] templateClassBytes = readTemplateClassBytes0();
-        //templateClassModel = ClassFile.of(LineNumbersOption.DROP_LINE_NUMBERS).parse(templateClassBytes);
-        // will want to drop line numbers in generated methods, but not the whole file?
 
-        // ClassModel is sealed, so cannot wrap it for shennanigans
-        // ConstantPoolBuilder is sealed too...
-        ClassModel templateClassModel = ClassFile.of().parse(templateClassBytes);
+        VisitorLists visitorLists = new VisitorLists();
 
-        _templateClassDesc = ClassDesc.of(templateClass.getName());
-        _generatedClassDesc = ClassDesc.of(generatedClassName);
+        ClassDesc templateClassDesc = ClassDesc.of(templateClass.getName());
+        ClassDesc generatedClassDesc = ClassDesc.of(generatedClassName);
+        visitorLists.visitors.add(new ChangeClassVisitor(templateClassDesc, generatedClassDesc));
 
-        dump(templateClassModel);
-        System.out.println();
-
-        // transformClass() does not include the ClassModel, instead iterating over its elements
-        System.out.println(templateClassModel);
-
-        // TODO! NEW_POOL better?
-        // Note that transformClass() delegates to build(), that might enable more control,
-        // perhaps to prevent the template appearing in the constant pool
-        // or to set templateClassEntry once up front.
-        //
-        // OR there are many methods on ClassTransform that could be overridden... and builder is using ClassTransform anyway...
-        //
-        // OR ClassRemapper? Looks to heavyweight for this case.
-        byte[] binaryRepresentation = ClassFile.of(ConstantPoolSharingOption.NEW_POOL)
-                .transformClass(templateClassModel, _generatedClassDesc, this::transformTemplateClass0);
-        //                .transformClass(templateClassModel, _generatedClassDesc, new ClassTransform() {
-        //                    @Override
-        //                    public void accept(ClassBuilder builder, ClassElement element) {
-        //                        transformTemplateClass0(builder, element);
-        //                        //this.con
-        //                    }
-        //                });
-
-        //        default byte[] transformClass(ClassModel model, ClassDesc newClassName, ClassTransform transform) {
-        //            return transformClass(model, TemporaryConstantPool.INSTANCE.classEntry(newClassName), transform);
-        //        }
-
-        //        @Override
-        //        public byte[] transformClass(ClassModel model, ClassEntry newClassName, ClassTransform transform) {
-        //            ConstantPoolBuilder constantPool = sharedConstantPool() ? ConstantPoolBuilder.of(model)
-        //                                                                    : ConstantPoolBuilder.of();
-        //            return build(newClassName, constantPool,
-        //                    new Consumer<ClassBuilder>() {
-        //                        @Override
-        //                        public void accept(ClassBuilder builder) {
-        //                            ((DirectClassBuilder) builder).setOriginal((ClassImpl)model);
-        //                            ((DirectClassBuilder) builder).setSizeHint(((ClassImpl)model).classfileLength());
-        //                            builder.transform((ClassImpl)model, transform);
-        //                        }
-        //                    });
-        //        }
-
-        //        default byte[] build(ClassDesc thisClass,
-        //                Consumer<? super ClassBuilder> handler) {
-        //ConstantPoolBuilder pool = ConstantPoolBuilder.of();
-        //return build(pool.classEntry(thisClass), pool, handler);
-        //}
-        // Takes ClassBuilder rather than ClassTransform, uses a new Pool
-        //ClassFile.of().build(_generatedClassDesc, this::blah);
-
-        // Not using ClassFile.transformClass() because we also want to transform "this" in the constant pool.
-        // It seems strange that transformClass() so not do so.
-        // transformClass() also delegates to build().
-        // This also MORE...
+        ClassModel classModel = ClassFile.of().parse(templateClassBytes);
+        byte[] binaryRepresentation = ClassFile.of().transformClass(classModel, generatedClassDesc, (b, e) -> {
+            visitClassElement(e, visitorLists, b);
+        });
 
         // temp - check that a second pass compresses the constant pool, removing references to the template.
         // looks OK - create a unit test to verify and make second pass configurable
@@ -181,7 +93,7 @@ public final class RuntimeGeneratedClassBuilder {
         //        .transformClass(secondPassModel, _generatedClassDesc, this::transformTemplateClass0);
 
         // temp - see what changed
-        System.out.println("----------------------");
+        //System.out.println("----------------------");
         dump(ClassFile.of().parse(binaryRepresentation));
 
         return ACCESSOR_CLASS_LOADER.loadClass(generatedClassName, binaryRepresentation);
@@ -194,98 +106,65 @@ public final class RuntimeGeneratedClassBuilder {
         }
     }
 
-    private void transformTemplateClass0(ClassBuilder builder, ClassElement element) {
-        if (element instanceof SourceFileAttribute attr) {
-            System.out.println("  " + element + "  {sourceFile=" + attr.sourceFile() + "}");
+    private void visitClassElement(ClassElement classElement, VisitorLists visitorLists, ClassBuilder classBuilder) {
+        ClassElement transformedElement = visit(classElement, visitorLists, (v, e) -> v.visitClassElement(e, classBuilder));
+        if (transformedElement == null) {
+            return;
         }
-        else if (element instanceof java.lang.classfile.Attribute attr) {
-            System.out.println("  " + element + "  (" + attr.getClass().getSimpleName() + ")");
-        }
-        else {
-            System.out.println("  " + element);
-        }
-
-        if (element instanceof MethodModel mm) {
-            builder.withMethod(mm.methodName(), mm.methodType(), mm.flags().flagsMask(), mb -> {
-                for (MethodElement me : mm) {
-                    transformTemplateMethod0(mb, me);
-                }
+        else if (transformedElement instanceof MethodModel mm) {
+            classBuilder.transformMethod(mm, (b, e) -> {
+                visitMethodElement(e, visitorLists, b);
             });
         }
         else {
-            builder.with(element);
+            classBuilder.with(transformedElement);
         }
     }
 
-    private void transformTemplateMethod0(MethodBuilder builder, MethodElement element) {
-        System.out.println("    " + element);
-
-        if (element instanceof CodeModel cm) {
-            builder.withCode(cb -> {
-                for (CodeElement ce : cm) {
-                    transformTemplateCode0(cb, ce);
-                }
+    private void visitMethodElement(MethodElement methodElement, VisitorLists visitorLists, MethodBuilder methodBuilder) {
+        MethodElement transformedElement = visit(methodElement, visitorLists, (v, e) -> v.visitMethodElement(e, methodBuilder));
+        if (transformedElement == null) {
+            return;
+        }
+        else if (methodElement instanceof CodeModel cm) {
+            methodBuilder.transformCode(cm, (b, e) -> {
+                visitCodeElement(e, visitorLists, b);
             });
         }
+    }
+
+    private void visitCodeElement(CodeElement codeElement, VisitorLists visitorLists, CodeBuilder codeBuilder) {
+        CodeElement transformedElement = visit(codeElement, visitorLists, (v, e) -> v.visitCodeElement(e, codeBuilder));
+        if (transformedElement == null) {
+            return;
+        }
         else {
-            builder.with(element);
+            codeBuilder.with(transformedElement);
         }
     }
 
-    private void transformTemplateCode0(CodeBuilder builder, CodeElement element) {
-        String indent = "      ";
-        String indentPlus = indent + "  > ";
-        System.out.println(indent + element);
+    private <ELEMENT extends ClassFileElement> ELEMENT visit(ELEMENT element, VisitorLists visitorLists, BiFunction<ClassFileElementVisitor, ELEMENT, ELEMENT> visitFunction) {
+        List<ClassFileElementVisitor> elementVisitors = visitorLists.getVisitorList(element.getClass());
+        if (elementVisitors.isEmpty()) {
+            // Quick exit for common case.
+            return element;
+        }
 
-        if (element instanceof FieldInstruction fi) {
-            if (isTemplate(fi.field().owner(), builder)) {
-                FieldRefEntry f = builder.constantPool().fieldRefEntry(_generatedClassDesc, fi.name().stringValue(), fi.typeSymbol());
-                element = FieldInstruction.of(fi.opcode(), _generatedClassEntry, fi.name(), fi.type());
-                System.out.println(indentPlus + element);
-            }
-        }
-        else if (element instanceof InvokeInstruction ii) {
-            if (isTemplate(ii.owner(), builder)) {
-                element = InvokeInstruction.of(ii.opcode(), _generatedClassEntry, ii.name(), ii.type(), false);
-                System.out.println(indentPlus + element);
-            }
-        }
-        else if (element instanceof LocalVariable lv) {
-            // Check type too in case the code is run again to display the class post-translation.
-            // type() has leading "L" and trailing semicolon.
-            if (lv.name().equalsString("this") && lv.typeSymbol().equals(_templateClassDesc)) {
-                ensureGeneratedClassEntry(builder);
-                element = LocalVariable.of(lv.slot(), "this", _generatedClassDesc, lv.startScope(), lv.endScope());
-                System.out.println(indentPlus + element);
+        ELEMENT result = element;
+        for (ClassFileElementVisitor elementVisitor : elementVisitors) {
+            // result = elementTransform.visit(result, ctx);
+            result = visitFunction.apply(elementVisitor, result);
+            if (result == null) {
+                return null;
             }
         }
 
-        // TEMP
-        if (element.toString().contains("_Template")) {
-            throw new IllegalStateException("Code needs modification to transform " + element);
-        }
+        // TODO! check that the type does not get changed by a visitor.
 
-        builder.with(element);
-        // and maybe  Attribute[name=SourceFile]  {sourceFile=MapAccessor_Template.java}
+        return result;
     }
 
-    private boolean isTemplate(ClassEntry classEntry, CodeBuilder builder) {
-        if (_templateClassEntry == null) {
-            ConstantPoolBuilder constantPoolBuilder = builder.constantPool();
-            Utf8Entry utf8 = constantPoolBuilder.utf8Entry(templateClassType);
-            _templateClassEntry = constantPoolBuilder.classEntry(utf8);
-            _generatedClassEntry = builder.constantPool().classEntry(_generatedClassDesc);
-        }
-        return classEntry.equals(_templateClassEntry);
-    }
-
-    private void ensureGeneratedClassEntry(CodeBuilder builder) {
-        if (_generatedClassEntry == null) {
-            _generatedClassEntry = builder.constantPool().classEntry(_generatedClassDesc);
-        }
-    }
-
-    // TEMP
+    // TEMP - create a DumpTransform (suggests Transform might not be the best name - we might just traverse)
     private void dump(ClassModel classModel) {
         for (PoolEntry poolEntry : classModel.constantPool()) {
             // toStrings() for the pool are scruffy
@@ -302,6 +181,21 @@ public final class RuntimeGeneratedClassBuilder {
             for (ClassFileElement subElement : iterableElement) {
                 dump0(indentSize + 1, subElement);
             }
+        }
+    }
+
+    private static class VisitorLists {
+        private List<ClassFileElementVisitor> visitors = new ArrayList<>();
+        // No key means not checked, empty list for checked and has no visitors.
+        private Map<Class<? extends ClassFileElement>, List<ClassFileElementVisitor>> visitorsForElement = new HashMap<>();
+
+        private List<ClassFileElementVisitor> getVisitorList(Class<? extends ClassFileElement> elementType) {
+            List<ClassFileElementVisitor> result = visitorsForElement.get(elementType);
+            if (result == null) {
+                result = visitors.stream().filter(t -> t.acceptsElement(elementType)).toList();
+                visitorsForElement.put(elementType, result);
+            }
+            return result;
         }
     }
 
---
+++ src/main/java/uk/co/magictractor/gradle/classvisitor/ClassFileElementVisitor.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.magictractor.gradle.classvisitor;
+
+import java.lang.classfile.ClassBuilder;
+import java.lang.classfile.ClassElement;
+import java.lang.classfile.ClassFileElement;
+import java.lang.classfile.CodeBuilder;
+import java.lang.classfile.CodeElement;
+import java.lang.classfile.MethodBuilder;
+import java.lang.classfile.MethodElement;
+
+// Transforms required:
+//
+// Change constants in code
+//   LoadConstant[OP=LDC, val=mockito]
+//
+// Change method name
+//   MethodModel[methodName=getTemplate, methodType=()Ljava/lang/Object;, flags=1]
+//
+// Maybe add synthetic flag to class and generated methods
+//  MethodModel[methodName=get, methodType=(Ljava/lang/String;)Ljava/lang/Object;, flags=1]
+//
+// Add marker interface with const that points to docs. (configurable)
+//   Interfaces[interfaces=]
+//
+// Write info to writer (similar to initial printlns)
+// Add java version for ClassFileVersion[majorVersion=69, minorVersion=0]
+// Add desc for AccessFlags[flags=33]
+public interface ClassFileElementVisitor {
+
+    // Used to quickly find relevant transforms rather than looping through all for every element.
+    // hmm, acceptsElement() better? - probably want ChangeThis done in a single transform
+    boolean acceptsElement(Class<? extends ClassFileElement> elementType);
+
+    /**
+     * <p>
+     * Returns given {@code ClassElement} if it is to be passed through without
+     * modification and subsequent {@code ClassFileElementVisitor}s applied.
+     * </p>
+     * <p>
+     * Returns a different instance if a change has been made to it and
+     * subsequent {@code ClassFileElementVisitor}s applied.
+     * </p>
+     * <p>
+     * Returns {@code null} if the {@code ClassElement} has already been handled
+     * using {@link ClassBuilder#with()} or is to be discarded.
+     * </p>
+     */
+    ClassElement visitClassElement(ClassElement element, ClassBuilder codeBuilder);
+
+    /**
+     * <p>
+     * Returns given {@code MethodElement} if it is to be passed through without
+     * modification and subsequent {@code ClassFileElementVisitor}s applied.
+     * </p>
+     * <p>
+     * Returns a different instance if a change has been made to it and
+     * subsequent {@code ClassFileElementVisitor}s applied.
+     * </p>
+     * <p>
+     * Returns {@code null} if the {@code MethodElement} has already been
+     * handled using {@link MethodBuilder#with()} or is to be discarded.
+     * </p>
+     */
+    MethodElement visitMethodElement(MethodElement element, MethodBuilder codeBuilder);
+
+    /**
+     * <p>
+     * Returns given {@code CodeElement} if it is to be passed through without
+     * modification and subsequent {@code ClassFileElementVisitor}s applied.
+     * </p>
+     * <p>
+     * Returns a different instance if a change has been made to it and
+     * subsequent {@code ClassFileElementVisitor}s applied.
+     * </p>
+     * <p>
+     * Returns {@code null} if the {@code CodeElement} has already been handled
+     * using {@link CodeBuilder#with()} or is to be discarded.
+     * </p>
+     */
+    CodeElement visitCodeElement(CodeElement element, CodeBuilder codeBuilder);
+
+}
