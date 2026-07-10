52d11fa59402772305cb2c7471393b15db844a58
Ken D <github@magictractor.co.uk>
2026-07-10 08:48:25 +0100

    Simplify settings in projects using magictractor-settings-plugin

--- src/main/java/uk/co/magictractor/gradle/MagicTractorSettingsPlugin.java
+++ src/main/java/uk/co/magictractor/gradle/MagicTractorSettingsPlugin.java
@@ -15,46 +15,35 @@
  */
 package uk.co.magictractor.gradle;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Map;
 
+import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.initialization.Settings;
 import org.gradle.api.initialization.dsl.VersionCatalogBuilder;
 import org.gradle.api.internal.catalog.parser.TomlCatalogFileParser;
-import org.gradle.api.invocation.Gradle;
 
 public abstract class MagicTractorSettingsPlugin implements Plugin<Settings> {
 
     private static final String MAGIC_TRACTOR_VERSION_CATALOG = "/gradle/magictractor.versions.toml";
     private static final Map<String, String> ZIP_FILE_SYSTEM_OPTIONS = Map.of("create", "true");
-
-    private static <T> T getGradleExtraProperty(Gradle gradle, String propertyKey) {
-        @SuppressWarnings("unchecked")
-        T propertyValue = (T) gradle.getExtensions().getExtraProperties().get(propertyKey);
-        if (propertyKey == null) {
-            // TODO! verify the syntax of the workaround ("this" is dubious")
-            // TODO! pass the "this" part as a param to this method
-            throw new IllegalStateException("Property not found. Either use " + MagicTractorSettingsPlugin.class.getSimpleName() +
-                    " or workaround by explicitly setting the property value in your settings file gradle.ext[\"" +
-                    propertyKey + "\"=this");
-        }
-        return propertyValue;
-    }
-
-    // and static setter? setter could check whether the prop has already set explicitly?
+    private static final String UNSET = "__unset__";
 
     @Override
     public void apply(Settings settings) {
-        URL url = getClass().getResource("/magictractor-settings-plugin.settings.gradle.kts");
-        settings.apply(act -> act.from(url));
+        settings.getRootProject().setName(UNSET);
+        settings.getRootProject().setBuildFileName(UNSET);
+
+        // TEMP
+        checkDeprecatedSettingsFiles(settings);
 
         // This plugin provides a version catalog for libs that are used in most projects
         // for logging, testing etc.
@@ -63,10 +52,64 @@ public abstract class MagicTractorSettingsPlugin implements Plugin<Settings> {
         // Looks for .toml files in the project. Not mandatory.
         configureProjectVersionCatalog(settings);
 
-        // A Map of reconciled version catalogs is created
-        // in MagicTractorPlugin where the Java language version
-        // is known and can be used to adjust dependencies used.
-        // See ReconciledLibrariesBuilder.
+        settings.getGradle().settingsEvaluated(s -> {
+            checkRootProjectName(s);
+            checkRootProjectBuildFileName(s);
+        });
+    }
+
+    private void checkDeprecatedSettingsFiles(Settings settings) {
+        StringBuilder failBuilder = new StringBuilder();
+
+        // These File checks are temporary. Remove once the few projects using this plugin are tidied.
+        File rootDir = settings.getRootDir();
+        if (new File(rootDir, "project.settings.gradle.kts").exists()) {
+            failBuilder.append("project.settings.gradle.kts is no longer used\n");
+        }
+        if (new File(rootDir, "project-local.settings.gradle.kts").exists()) {
+            failBuilder.append("project-local.settings.gradle.kts is no longer used\n");
+        }
+
+        if (!failBuilder.isEmpty()) {
+            throw new GradleException(failBuilder.toString());
+        }
+    }
+
+    // An explicit rootProject.name is best practice.
+    // See https://docs.gradle.org/current/userguide/best_practices_general.html#name_your_root_project.
+    private void checkRootProjectName(Settings settings) {
+        if (UNSET.equals(settings.getRootProject().getName())) {
+            String suggested = settings.getRootDir().getName();
+            throw new GradleException("Settings must include an explicit root project name, such as rootProject.name = \"" + suggested + "\"\n");
+        }
+    }
+
+    /**
+     * <p>
+     * The build file name should be explicitly configured using
+     * {@code rootProject.buildFileName=<name>}.
+     * </p>
+     * <p>
+     * When working with multiple projects, finding a specific settings file can
+     * be tricky if they are all called {@code build.gradle.kts}.
+     * </p>
+     * <p>
+     * My preference is to use the last part of the root project name plus
+     * {@code ".gradle.kts"}. I decided against having the plugin set that
+     * automatically so that anybody unfamiliar with my non-standard preference
+     * can get a clue about what is happening.
+     * </p>
+     */
+    // TODO! Gradle property to turn this off
+    private void checkRootProjectBuildFileName(Settings settings) {
+        if (UNSET.equals(settings.getRootProject().getBuildFileName())) {
+            // Could use a StringBuilder, but I suspect that the errors will be centralised so they can be documented.
+            // They also need associated properties to turn options off or reduce severity.
+            String rootProjectName = settings.getRootProject().getName();
+            int lastHyphenIndex = rootProjectName.lastIndexOf("-");
+            String suggested = ((lastHyphenIndex == -1) ? rootProjectName : rootProjectName.substring(lastHyphenIndex + 1)) + ".gradle.kts";
+            throw new GradleException("Settings must include an explicit root project build file name, such as rootProject.buildFileName = \"" + suggested + "\"\n");
+        }
     }
 
     private void configureMagictractorLibs(Settings settings) {
